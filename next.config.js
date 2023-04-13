60a158571cf18b0b41a41884d8ec00c322061f2e
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-12 21:28:48 -0400

    get it all wired up and working for nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.wasm/,
+      type: 'asset/resource'
+    });
+    return config;
+  }
 }
 
 module.exports = nextConfig
