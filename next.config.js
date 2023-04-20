6b42614b8c157c4dc97207b1c19be3dfda05ba38
Matt <1009003+tantaman@users.noreply.github.com>
2023-04-20 06:50:10 -0400

    disable strict mode until we uncover the bug for #1

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   webpack: (config, { isServer }) => {
     config.module.rules.push({
       test: /\.wasm/,
-      type: 'asset/resource'
+      type: "asset/resource",
     });
     return config;
-  }
-}
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
